b5708b545591c510da3312c540922e4698fd8fb3
Ryan Hinkle <hinkle.ryan97@gmail.com>
2022-02-03 08:39:30 -0800

    commit tree a07d1ad733bb583f0ab306b1fe8eb430395875b0

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const block = 264;
+
+function distanceFromHqInBlocks(distanceInBlocks) {
+    return Math.abs(distanceInBlocks - 42);
+  }
+
+  function distanceFromHqInFeet(distanceInBlocks) {
+    return distanceFromHqInBlocks(distanceInBlocks) * block;
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    return (Math.abs(start - destination) * block);
+  }
+
+  function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+    } else if (distanceTravelledInFeet(start, destination) > 2000) {
+        return 25;
+    } else if (distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+    } else (distanceTravelledInFeet(start, destination) > 2500)
+        // console.log("num25: ", 264);
+        return 'cannot travel that far';
+  }
